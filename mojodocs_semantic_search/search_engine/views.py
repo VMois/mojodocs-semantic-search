e20eb4be8ba2e8781b5cdd8deec6f9badd379c68
VMois <vmois@protonmail.com>
2024-01-23 22:53:48 +0100

    Add error handling for ChromaDB query when searching

--- mojodocs_semantic_search/search_engine/views.py
+++ mojodocs_semantic_search/search_engine/views.py
@@ -1,12 +1,17 @@
+import logging
+
 from chromadb import HttpClient
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 
-from django.http import HttpResponseBadRequest
+from django.http import HttpResponseBadRequest, HttpResponseServerError
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
 from .forms import QueryForm
+
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
@@ -18,15 +23,21 @@ def index(request):
 def search(request):
     query = QueryForm(request.POST)
     if query.is_valid():
-        client = HttpClient('http://chromadb:8000')
+        results = []
+        try:
+            client = HttpClient('http://chromadb:8000')
 
-        db = Chroma(
-            client=client,
-            embedding_function=OpenAIEmbeddings(),
-        )
+            db = Chroma(
+                client=client,
+                embedding_function=OpenAIEmbeddings(),
+            )
 
-        docs = db.similarity_search(query.cleaned_data['content'])
-        results = [{'origin_url': doc.metadata['source_link'], 'matched_content': doc.page_content} for doc in docs[:min(5, len(docs))]]
+            docs = db.similarity_search(query.cleaned_data['content'])
+            results = [{'origin_url': doc.metadata['source_link'], 'matched_content': doc.page_content} for doc in docs[:min(5, len(docs))]]
+        except Exception as e:
+            logger.error(e)
+            return render(request, 'general_error.html', {'message': "Some unexpected error happened. Couldn't perform a search."}, status=HttpResponseServerError.status_code)
+
         return render(request, 'search_results.html', {'results': results})
     else:
         return render(request, 'form_errors.html', {'form': query}, status=HttpResponseBadRequest.status_code)
