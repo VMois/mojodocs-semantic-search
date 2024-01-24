54dd0df307d95500f3393cc8e3e6343960de3c74
VMois <vmois@protonmail.com>
2024-01-24 23:34:04 +0100

    Add script to pull changes from repo and restart docker-compose

---
+++ update_and_restart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BRANCH="main"
+
+git pull origin "$BRANCH"
+
+# Rebuild Docker Compose images
+docker-compose build
+
+# Restart/Start all Docker Compose services
+docker-compose up -d
