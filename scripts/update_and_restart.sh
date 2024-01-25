2bef6d2bde27b8a5095c80d0bb1fb8d390563fe3
VMois <vmois@protonmail.com>
2024-01-25 02:54:18 +0100

    Fix update and restart bash script when docker compose command is not correct

--- scripts/update_and_restart.sh
+++ scripts/update_and_restart.sh
@@ -5,7 +5,7 @@ BRANCH="main"
 git pull origin "$BRANCH"
 
 # Rebuild Docker Compose images
-docker-compose build
+docker compose build
 
 # Restart/Start all Docker Compose services
-docker-compose up -d
+docker compose up -d
