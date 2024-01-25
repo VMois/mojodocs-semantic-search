82c17fee42480b87f2fbb06332aca6f213f0d6fa
VMois <vmois@protonmail.com>
2024-01-25 02:20:42 +0100

    Allow any host to connect on PROD and allow semojo.vmois.dev to be trusted CSRF origin

--- mojodocs_semantic_search/mojodocs_semantic_search/settings_prod.py
+++ mojodocs_semantic_search/mojodocs_semantic_search/settings_prod.py
@@ -2,4 +2,6 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'mojodocs.vmois.dev']
+# We are behing a reverse proxy, so we can allow all hosts
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://semojo.vmois.dev']
